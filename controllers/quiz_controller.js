6e5e2124fd0274e991f53dcc961bf18b38701a9f
metal347 <dros001@yahoo.com>
2015-07-01 16:45:32 +0200

    Crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -22,7 +22,7 @@ exports.index = function(req, res) {
     search = "%" + search.replace(/ /g, "%") + "%";
     
     // buscamos las preguntas ordenadas alfabéticamente por el campo "pregunta"
-    models.Quiz.findAll({where: ["pregunta like ?", search], order: "pregunta"}).then(
+    models.Quiz.findAll({where: ["lower(pregunta) like lower(?)", search], order: "pregunta"}).then(
       function(quizes) {
         res.render('quizes/index', { quizes: quizes});
       }
@@ -50,4 +50,23 @@ exports.answer = function(req, res) {
     resultado = 'Correcto';
   }
   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');  
+  })   // res.redirect: Redirección HTTP a lista de preguntas
 };
